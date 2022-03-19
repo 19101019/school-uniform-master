e95c59cebdd4a733740d17b4f9379ea8a6bd9580
Swati Mandal <Swatimandal01@gmail.com>
2022-03-19 14:34:33 +0530

    done

--- school-uniform-master/src/components/Header/Header.tsx
+++ school-uniform-master/src/components/Header/Header.tsx
@@ -1,5 +1,3 @@
-import PersonIcon from '@mui/icons-material/PersonOutlineOutlined';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCartOutlined';
 import "./Header.scss"
 const Header = () => {
   return (
